3fc2f0f54edbd21315ab1687cc02eba3c625b849
Arnav Katyayan <ishumisra99@gmail.com>
2025-03-06 20:45:59 +0530

    Light mode dark mode changes .

--- src/App.js
+++ src/App.js
@@ -4,28 +4,46 @@ import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import HomePage from './HomePage';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import { Switch, FormControlLabel } from "@mui/material";
+import ReactSwitch from 'react-switch';
+import { dark } from '@mui/material/styles/createPalette';
 function App() {
-  const [homeBtnClicked, setHomeBtnClicked] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
-  const handleHome = () => {
-    setHomeBtnClicked(true);
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
   };
 
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   return (
-    <div>
-      <Navbar bg="light" expand="lg" className='nav-bg'>
+    <div className={darkMode ? "dark-theme" : "light-theme"}>
+      
+      <Navbar bg={darkMode ? "dark" : "light"} expand="lg" className='nav-bg'>
         <Container>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto margin-right-nav">
-              <Nav.Link onClick={handleHome}>Home</Nav.Link>
-              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"}>Home</Nav.Link>
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"} href="#">About</Nav.Link>
+              <div className="dark-mode-btn">
+                {darkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}
+                <ReactSwitch onChange={toggleDarkMode} checked={darkMode}/>
+              </div>
+              
+             
             </Nav>
             <Nav className="me-auto margin-left-nav">
-              <Nav.Link href="#">Data Structures & Algo</Nav.Link>
-              <Nav.Link href="#">Projects</Nav.Link>
-              <Nav.Link href="#">Contact</Nav.Link>
+            
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"} href="#">Data Structures & Algo</Nav.Link>
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"} href="#">Projects</Nav.Link>
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"} href="#">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -34,10 +52,10 @@ function App() {
       <HomePage />
 
       <footer>
-        <Navbar bg="dark" variant="dark" fixed="bottom">
+        <Navbar bg={darkMode ? "dark" : "light"} variant={darkMode ? "dark" : "light"} fixed="bottom">
           <Container className="justify-content-center">
             <Nav>
-              <Nav.Link>Version 1.0.0</Nav.Link>
+              <Nav.Link className={darkMode ? "dark-theme" : "light-theme"}>Version 1.0.0</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
